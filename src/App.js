3b8d71ce60cbc4589d743b0a3f51572181d75db6
Subham <subhamswaruppradhan2580@gmail.com>
2023-01-05 00:25:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [fullname, setFullname] = useState({
+    fName:"",
+    lName:"",
+    email:""
+  });
+  function updateFullname(event){
+
+    // const newValue = event.target.value;
+    // const input = event.target.name;
+    const {value:newValue,name:input}=event.target;
+    
+    
+        
+    // if(input === "fName"){
+    //   setFullname({fname:newValue});
+    // }else if(input === "lName"){
+    //   setFullname({lname:newValue});
+    // }
+
+    // setFullname((prevValue)=>{
+    //   if(input === "fName"){
+    //     return{
+    //       fName:newValue,
+    //       lName:prevValue.lName,
+    //       email:prevValue.email
+    //     }
+    //   }else if(input === "lName"){
+    //     return{
+    //         fName:prevValue.fName,
+    //         lName:newValue,
+    //         email:prevValue.email
+    //       }
+    //   }else if(input === "email"){
+    //     return{
+    //         fName:prevValue.fName,
+    //         lName:prevValue.lName,
+    //         email:newValue
+    //       }
+    //   }  
+    // })
+
+    
+    setFullname((prevValue)=>({...prevValue,[input]:newValue}));
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <div>Hello {fullname.fName} {fullname.lName} {fullname.email}</div>
+      <input name="fName" onChange={updateFullname} ></input>
+      <input name="lName" onChange={updateFullname} ></input>
+      <input name="email" onChange={updateFullname} ></input>
+      <br></br>
+      <button>Click me</button>
     </div>
   );
 }
